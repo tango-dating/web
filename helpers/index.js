91e22ee7241f395a6d946e6f31e8d038a2a9dc24
Viktor A. Rozenko Voitenko <sharp.vik@gmail.com>
2022-05-24 09:44:27 +0100

    chore(*): add firebase config

---
+++ helpers/index.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyADbuphHuhoi1l8je7C5ED0MMr-JPfkAz0",
+  authDomain: "tango-dating.firebaseapp.com",
+  projectId: "tango-dating",
+  storageBucket: "tango-dating.appspot.com",
+  messagingSenderId: "481562476129",
+  appId: "1:481562476129:web:975ddc701692c5deeb3420",
+  measurementId: "G-NNCKGE0YRC"
+}
